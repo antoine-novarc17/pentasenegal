33ff193ed554dd6f55cb14de7bbcbe46dc387ae3
antoine-novarc17 <antoine.gros@networksglobal.com>
2026-08-07 10:23:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ const contactForm = document.getElementById('contact-form');
 const formNote = document.getElementById('form-note');
 
 
-const CONTACT_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzv3kaDjqTYi2dVztMKK5AjgkhhFJNFwNFa_7rJG3-fRx_FTjGn_W9t1v_6IUc2CMJL/exec";
+const CONTACT_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzisPjhq8UW0PX7lIeg1opVPkDbEgEdTjjY6xoruUmbw5PyBhgI0TETQHeRejFBg1IbRQ/exec";
 
 
 if(contactForm){
